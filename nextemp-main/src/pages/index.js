81a1cfbd8282109c3a3cd7ef416c07c6ecee659f
Mark Otuya <markotuya0@gmail.com>
2025-04-04 07:42:29 +0100

    new directory

---
+++ nextemp-main/src/pages/index.js
@@ -0,0 +1,85 @@
+import AnimatedText from "@/components/AnimatedText";
+import { HireMe } from "@/components/HireMe";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import TransitionEffect from "@/components/TransitionEffect";
+import profilePic from "../../public/images/profile/mark1.png";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Mark Otuya Portoflio</title>
+        <meta name="description" content="My Personal Web Development Portfolio" />
+      </Head>
+
+      <TransitionEffect />
+      <article
+        className={`flex min-h-screen items-center text-dark dark:text-light sm:items-start`}>
+        <Layout className="!pt-24 md:!pt-16 sm:!pt-28">
+          <div className="flex w-full items-start justify-between md:flex-col pt-10 pb-24">
+            <div className="w-1/2 lg:hidden md:flex flex self-center max-h-fit">
+              {
+                <Image
+                  priority={true}
+                  src={profilePic}
+                  alt="image"
+                  className="h-auto w-100"
+                  height={340}
+                  width={340}
+                  sizes="33vw"
+                />
+              }
+            </div>
+            <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
+              <AnimatedText
+                text="Hey, I’m Mark"
+                className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+              />
+              <div className="flex w-1/2 w-full items-center items-start lg:w-full  lg:!justify-center  sm:!justify-center  md:!text-center md:inline-block md:w-full">
+                <h2 className="animate-text bg-gradient-to-r from-lightGreen via-lightGreen to-slideGreen bg-clip-text text-transparent font-semibold capitalize !text-5xl xl:!text-4xl lg:!text-4xl md:!text-5xl sm:!text-3xl">
+                  I create engaging web experiences.
+                </h2>
+              </div>
+
+              <p className="my-4 text-base font-medium md:text-sm sm:!text-sm">
+                I’m a Full-Stack Developer with over 4 years of experience building innovative and
+                user-friendly web applications. Proficient in JavaScript, React, Node.js,
+                TypeScript, and Postgresql, I specialize in designing scalable architectures and
+                delivering high-quality solutions. I’m skilled in both front-end and back-end
+                development, REST and GraphQL API design, and database management with PostgreSQL
+                and MongoDB. Passionate about clean code, performance optimization, and creating
+                seamless user experiences, I thrive in agile, collaborative environments..
+              </p>
+              <div className="mt-2 flex items-center self-start gap-3 grid-cols-2 lg:self-center">
+                <Link
+                  href="/about/"
+                  target={"_self"}
+                  className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
+            capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+            dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+            md:p-2 md:px-4 md:text-base
+             `}>
+                  About Me
+                </Link>
+                <Link
+                  href="/projects/"
+                  target={"_self"}
+                  className={`flex items-center rounded-lg border-2 border-solid bg-light p-2.5 px-6 text-lg font-semibold
+            capitalize text-dark hover:border-light hover:bg-dark hover:text-light 
+            dark:bg-dark dark:text-light dark:hover:bg-light dark:hover:text-dark
+            md:p-2 md:px-4 md:text-base
+             `}>
+                  projects
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
+        <HireMe />
+      </article>
+    </>
+  );
+}
